66091a2487b2271bb876d950ad31e90e380d212e
moondev03 <moondev03@gmail.com>
2024-07-09 15:49:28 +0900

    Navigation 실습 코드

---
+++ Navigation/app/src/main/java/com/example/navigation/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.navigation
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.navigation.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
