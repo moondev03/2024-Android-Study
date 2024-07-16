b604ca9f76bd539fc81cc5184631db2dedd1fce6
moondev03 <moondev03@gmail.com>
2024-07-16 15:14:56 +0900

    [3주차] Navigation2 실습 코드

---
+++ Navigation2/app/src/main/java/com/example/navigation2/TwoFragment.kt
@@ -0,0 +1,37 @@
+package com.example.navigation2
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.navigation2.databinding.FragmentTwoBinding
+
+class TwoFragment : Fragment() {
+
+    private var _binding: FragmentTwoBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentTwoBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            btnOne.setOnClickListener {
+                findNavController().navigate(R.id.action_twoFragment_to_oneFragment)
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+    }
+}
