699677101a85e16d75dd703e2daea49cf8b14e9a
MunJangHun <105299421+moondev03@users.noreply.github.com>
2024-07-16 15:15:49 +0900

    [3주차] Navigation2 실습 코드 (#8)

---
+++ Navigation2/app/src/main/java/com/example/navigation2/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.example.navigation2
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.navigation2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 네비게이션 연결
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        // 바텀 네비게이션 연결
+        binding.bottomNavi.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            // 바텀 네비게이션이 표시되는 Fragment
+            if(destination.id == R.id.bottomOneFragment || destination.id == R.id.bottomTwoFragment
+                || destination.id == R.id.bottomThreeFragment){
+                binding.bottomNavi.visibility = View.VISIBLE
+            }
+            // 바텀 네비게이션이 표시되지 않는 Fragment
+            else{
+                binding.bottomNavi.visibility = View.GONE
+            }
+        }
+
+        // 앱 바 생성
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    // 뒤로가기
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    var waitTime = 0L
+    override fun onBackPressed() {
+        if(navController.currentDestination?.id == R.id.bottomOneFragment) {
+            if (System.currentTimeMillis() - waitTime >= 1500) {
+                waitTime = System.currentTimeMillis()
+                Toast.makeText(this, "뒤로가기 버튼을 한번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show()
+            } else {
+                finish()
+            }
+        }
+        else{
+            super.onBackPressed()
+        }
+    }
+}
---
+++ Navigation2/app/src/main/java/com/example/navigation2/OneFragment.kt
@@ -0,0 +1,40 @@
+package com.example.navigation2
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.navigation2.databinding.FragmentOneBinding
+
+class OneFragment : Fragment(R.layout.fragment_one) {
+
+    private var _binding : FragmentOneBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        _binding = FragmentOneBinding.bind(view)
+
+        // 액션 연결
+        binding.apply {
+            // 두번째 화면으로 이등
+            button.setOnClickListener {
+                val action = R.id.action_oneFragment_to_twoFragment
+                findNavController().navigate(action)
+            }
+
+            // 세번째 화면으로 이동
+            submitButton.setOnClickListener {
+                val user = User(nameEditView.text.toString(),ageEditView.text.toString().toInt())
+                val action = OneFragmentDirections.actionOneFragmentToThreeFragment(user = user, name = "Han")
+                findNavController().navigate(action)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
